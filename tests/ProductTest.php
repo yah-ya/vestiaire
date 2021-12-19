a75c5d3059d00d833519b8efc34680831d1334ed
Yahyya <y.t.15132@gmail.com>
2021-12-19 19:03:36 +0330

    Added new test to make sure sum amounts of transactions are equal to sum of products prices

--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -19,6 +19,29 @@ class ProductTest extends TestCase
     }
 
 
+    public function test_if_sum_of_transactions_amount_equal_to_sum_of_products_price(){
+        \Illuminate\Support\Facades\Artisan::call('db:seed', ['--class' => 'CurrenciesTableSeeder']);
+        $products = \App\Models\Product::factory()->count(5)->create();
+        $sumOfProducts = $this->CalcSumOfProductPrices($products);
+
+        $this->json('post','/transactions',['products'=>$products,'sellerId'=>1])
+            ->seeJson([
+                'res' => true,
+            ]);
+
+        $controller = new \App\Http\Controllers\TransactionController();
+        $splitedProducts = $controller->splitProducts($products);
+        $sumOfTransactions = 0;
+        foreach($splitedProducts as $currency=>$sp){
+            foreach($sp as $tr){
+                $sumOfTransactions += $tr['sum'];
+            }
+        }
+
+        $this->assertEquals($sumOfProducts,$sumOfTransactions,'Sum of products prices are not equal to Sum of transactions amount');
+
+    }
+
     private function CheckTransactionsInDatabase($products){
         $controller = new \App\Http\Controllers\TransactionController();
         $products = \App\Http\Controllers\ProductController::initProducts($products);
@@ -40,5 +63,13 @@ class ProductTest extends TestCase
         foreach($whatToSee as $ws) {
             $this->seeInDatabase('transactions', $ws);
         }
+    }
+
+    private function CalcSumOfProductPrices($products){
+        $sum = 0;
+        foreach($products as $product){
+            $sum += $product->price;
+        }
+        return $sum;
     }
 }
